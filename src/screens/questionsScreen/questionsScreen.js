42d46ec28b9a1f830a31e31539da7289fc4de457
saifmoh7 <saif.2017.moh@gmail.com>
2022-08-06 22:24:44 +0300

    timer in question screen

--- src/screens/questionsScreen/questionsScreen.js
+++ src/screens/questionsScreen/questionsScreen.js
@@ -1,18 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { FlatList, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { FlatList, Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import Icon from '../../components/icons';
 import { getQuestionsList } from '../../utiles/database';
 import styles from './style';
+const win = require('../../images/win.png')
+const fail = require('../../images/fail.png')
 
 function QuestionsScreen({navigation, route}) {
 
   const examId = route.params.examId;
-  const timer = route.params.timer
+  const time = route.params.timer*60
   const noOfQ = route.params.noOfQ
+
+  const intervalRef = useRef(null);
 
   const [refreshing, setRefreshing] = useState(false)
   const [questions, setQuestions] = useState([]);
   const [submit, setSubmit] = useState(false);
+  const [showResults, setShowResults] = useState(false);
+  const [timer, setTimer] = useState("00:00:00");
+  const [examTime, setExamTime] = useState(0);
 
   const [state, setstate] = React.useReducer((prevState, data) => { return { ...prevState, ...data } }, {correctCount : 0, incorrectCount : 0, notattmpeted : parseInt(noOfQ), questionsState : {} });
 
@@ -20,7 +27,6 @@ function QuestionsScreen({navigation, route}) {
     setRefreshing(true)
     try {
       const questionsList = await getQuestionsList(examId)
-      console.log(questionsList)
 
       let tempQuestions = []
 
@@ -28,14 +34,11 @@ function QuestionsScreen({navigation, route}) {
 
         question.options = shuffleArray([question.option_1, question.option_2, question.option_3, question.option_4])
 
-        console.log({question})
-
         tempQuestions.push(question)
       });
       
       let tempQues = shuffleArray([...tempQuestions])
       
-      console.log(tempQues)
       setQuestions(tempQues)
 
       setRefreshing(false)
@@ -54,6 +57,57 @@ function QuestionsScreen({navigation, route}) {
     }
     return array;
   };
+
+  function examTimer(examTime){
+    let sec = Math.floor( (examTime) % 60 );
+    let min = Math.floor( (examTime/60) % 60 );
+    let hours = Math.floor( (examTime/(60*60)) % 24 );
+    console.log(examTime,hours,min,sec)
+    let time = (hours > 9 ? hours : '0' + hours) + ':' + (min > 9 ? min : '0' + min) + ':' + (sec > 9 ? sec : '0' + sec)
+    return time.toString()
+  }
+
+  function getDeadlineTime(){
+    let dateline = new Date();
+    dateline.setSeconds(dateline.getSeconds()+time);
+    return dateline;
+  }
+
+  function clearTimer(endtime){
+    setTimer("00:00:00");
+    if (intervalRef.current) clearInterval(intervalRef.current);
+    let count = 0
+    const id = setInterval(() => {
+      startTimer(endtime)
+      count = count+1
+      setExamTime(count)
+    },1000)
+    intervalRef.current = id;
+  }
+
+  function startTimer(deadline){
+    let{total, hours, min, sec} = getTimeRemaining(deadline);
+    if (total >= 0 && !submit) {
+      setTimer(
+        (hours > 9 ? hours : '0' + hours) + ':' + (min > 9 ? min : '0' + min) + ':' + (sec > 9 ? sec : '0' + sec)
+      );
+    } else {
+      clearInterval(intervalRef.current);
+      setSubmit(true)
+      setShowResults(true)
+      console.log("timeup")
+    }
+  }
+
+  function getTimeRemaining(endtime){
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const sec = Math.floor( (total/1000) % 60 );
+    const min = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/1000*60*60) % 24 );
+    return {
+      total, hours, min, sec
+    }
+  }
 
   const choiceIcon = (index)=> {
     if(index === 0){
@@ -107,8 +161,11 @@ function QuestionsScreen({navigation, route}) {
   }
 
   useEffect(() => {
-    console.log(examId)
     getQuestions(examId)
+
+    clearTimer(getDeadlineTime());
+
+    return() => {if (intervalRef.current) clearInterval(intervalRef.current)};
   },[])
 
   return (
@@ -118,8 +175,8 @@ function QuestionsScreen({navigation, route}) {
           <Text style={{fontSize: 18, color: '#ffffff'}}>API 653 Exam</Text>
         </View>
         <View style = {{...styles.headerBar}}>
-          <Text>Not Attampeted/Total Ques. is 5/8</Text>
-          <Text>00:00:00 sec</Text>
+          <Text>Not Attampeted/Total Ques. is {state.notattmpeted}/{noOfQ}</Text>
+          <Text>{timer} sec</Text>
         </View>
       </View>
 
@@ -130,44 +187,60 @@ function QuestionsScreen({navigation, route}) {
         showsVerticalScrollIndicator={false}
         keyExtractor={(item, i) => i}
         style = {{...styles.questionsContainer}}
-        renderItem = {({item: question}) => (
-            <View key={question.question.index} style = {{...styles.questionContainer}}>
-              <View style = {{...styles.question}}>
-                <Text style={{fontSize: 16, color: '#ffffff'}}>Q{question.index + 1}: {question.question}</Text>
-              </View>
-              <View style = {{...styles.optionsContainer}}>
-                  {
-                    question.options.map((option, optionIndex) => {
-                      return(
-                      <TouchableOpacity
-                      key={optionIndex}
-                      onPress={() => {
-                        let tempQuestions = [...questions];
-                        tempQuestions[question.index].selectedOption = option;
-                        console.log(tempQuestions[question.index])
-                        setQuestions([...tempQuestions]);
-                      }}
-                      style = {{...styles.optionContainer, backgroundColor: getOptionBg(option, question.correctAnswer, question.selectedOption)}}
-                      >
-                        <View style = {{...styles.choice}}>
-                          <Text style={{fontSize: 30, color: '#ffffff'}}>{choiceIcon(optionIndex)}</Text>
-                        </View>
-                        <View style = {{...styles.option}}>
-                          <Text style={{fontSize: 14, color: '#000000'}}>{option}</Text>
-                        </View>
-                      </TouchableOpacity>
-                    )})
-                  }
-                {
-                  submit ? 
-                    <View style = {{...styles.ref}}>
-                      <Text style={{fontSize: 16}}>General (API 650 1.1.1)</Text>
-                    </View> : <React.Fragment></React.Fragment>
-                }
-              </View>
-              <View style = {{...styles.line}}></View>
-            </View>
-        )}
+        renderItem = {({item: question}) => {
+          if (question.index < noOfQ) {
+            return <View key={question.index} style = {{...styles.questionContainer}}>
+                    <View style = {{...styles.question}}>
+                      <Text style={{fontSize: 16, color: '#ffffff'}}><Text style = {{color: state.questionsState.hasOwnProperty(question.index.toString()) ? '#28AFEA' : '#ffffff'}}>Q{question.index + 1}</Text> : {question.question}</Text>
+                    </View>
+                    <View style = {{...styles.optionsContainer}}>
+                        {
+                          question.options.map((option, optionIndex) => {
+                            return(
+                            <TouchableOpacity
+                            disabled={submit}
+                            key={optionIndex}
+                            onPress={() => {
+                              console.log(intervalRef.current)
+                              let {correctCount, incorrectCount, notattmpeted, questionsState} = state
+                              
+                              if ( !questionsState.hasOwnProperty(String(question.index)) ) {
+                                notattmpeted = notattmpeted-1
+                              }
+
+                              questionsState[question.index] = ( option === question.correctAnswer )
+
+                              correctCount = Object.values(questionsState).filter(bool=>bool==true).length;
+                              incorrectCount = Object.values(questionsState).filter(bool=>bool==false).length;
+                              
+                              setstate({correctCount, incorrectCount, notattmpeted, questionsState})
+
+                              let tempQuestions = [...questions];
+                              tempQuestions[question.index].selectedOption = option;
+                              setQuestions([...tempQuestions]);
+                            }}
+                            style = {{...styles.optionContainer, backgroundColor: getOptionBg(option, question.correctAnswer, question.selectedOption)}}
+                            >
+                              <View style = {{...styles.choice}}>
+                                <Text style={{fontSize: 30, color: '#ffffff'}}>{choiceIcon(optionIndex)}</Text>
+                              </View>
+                              <View style = {{...styles.option}}>
+                                <Text style={{fontSize: 14, color: '#000000'}}>{option}</Text>
+                              </View>
+                            </TouchableOpacity>
+                          )})
+                        }
+                      {
+                        submit ? 
+                          <View style = {{...styles.ref}}>
+                            <Text style={{fontSize: 16}}>General (API 650 1.1.1)</Text>
+                          </View> : <React.Fragment></React.Fragment>
+                      }
+                    </View>
+                    <View style = {{...styles.line}}></View>
+                  </View>
+          } else {<React.Fragment></React.Fragment>}
+        }}
       />
       <View style = {{...styles.iconContainer}}>
         <Icon
@@ -175,10 +248,43 @@ function QuestionsScreen({navigation, route}) {
               size = {60}
               color = "#ffffff"
               onPress = {() => {
+                setShowResults(true)
                 setSubmit(true)
+                clearInterval(intervalRef.current)
               }}
           />
       </View>
+      {
+        showResults ? 
+          <View style = {{...styles.resultsContainer}}>
+            <Image
+            resizeMode="contain"
+            source={(state.correctCount*100/noOfQ) >= 50 ? win : fail}
+            style = {{width : "80%", height : "20%"}}
+            />
+            <Text>
+              {state.correctCount*100/noOfQ}% Score
+            </Text>
+            <Text>
+              Total Questions: {noOfQ}
+            </Text>
+            <Text>
+              Question Not Attmpeted: {state.notattmpeted}
+            </Text>
+            <Text>
+              Correct Awnser: {state.correctCount}
+            </Text>
+            <Text>
+              Incorrect Awnser: {state.incorrectCount}
+            </Text>
+            <Text>
+              Total Time: {examTimer(time)}
+            </Text>
+            <Text>
+              Spend Time: {examTimer(examTime)}
+            </Text>
+          </View> : <React.Fragment></React.Fragment>
+      }
     </SafeAreaView>
   )
 }
